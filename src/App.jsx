448118d423b8b95e75d98b9fd66416f300bbe9f0
jairam kumar <jairamk120@gmail.com>
2024-09-26 16:26:06 +0530

    adding button

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
 const App = () => {
-  return (
-    <div  className='bg-red-600 px-3 py-4 inline-block rounded'>App  hello</div>
+  return (<>
+ 
+ <div  className='bg-red-600 px-3 py-4 inline-block rounded'>App  hello</div>
+    <button className='px-2 py-1 bg-blue-800 rounded' > click me</button> 
+  </>
   )
 }
 
