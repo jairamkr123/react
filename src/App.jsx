c8f65e054047e94dc45122e4fde2bba8bbdf2ff0
jairam kumar <jairamk120@gmail.com>
2024-09-26 16:28:41 +0530

    Initial commit with project setup and user authentication

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ const App = () => {
  
  <div  className='bg-red-600 px-3 py-4 inline-block rounded'>App  hello</div>
     <button className='px-2 py-1 bg-blue-800 rounded' > click me</button> 
+    <button className='px-2 py-1 bg-yellow-800 rounded' > click me</button> 
+    <button className='px-2 py-1 bg-red-800 rounded' > click me</button> 
   </>
   )
 }
